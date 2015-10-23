9ae561fc14210cd167cc23abc24dcc4f78685c37
Erin Mahon <erin.mahon14@gmail.com>
2015-10-23 13:24:20 -0400

    delete post option completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,20 @@ class PostsController < ApplicationController
 def index  
   	@posts = Post.all
   end  
+  def show
+  	@post = Post.find(params[:id])
+  end
   def update  
   end
+  def destroy
+  	@post = Post.find(params[:id])
+  	if @post.destroy
+  	flash[:notice] = "Post deleted successfully."
+  	else
+  	flash[:alert] = "The was a problem deleting the post."	
+  	end
+  	redirect_to :back
+  end
 end
+
+
