9a93db619edcbf7ef1749357d5847e2f9ee557c5
Erin Mahon <erin.mahon14@gmail.com>
2015-10-23 17:25:37 -0400

    initialized further functionality set up home page sign up and log in forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,35 @@ class UsersController < ApplicationController
   def index  
   	@users = User.all
   end  
+  def show
+  	@user = User.find(params[:id])
+  end
+  def new
+  	@user = User.new
+  end
+  def create
+    @user = User.create(user_params)
+    redirect_to users_path
+  end
+  def edit
+  	@user = User.find(params[:id])
+  end
   def update  
+  	@user = User.find(params[:id]).update(user_params)
+  	redirect_to users_path
+  end
+  def destroy
+  	@user = User.find(params[:id])
+  	if @user.destroy
+  	  flash[:notice] = "User deleted successfully."
+  	else
+  	  flash[:alert] = "The was a problem deleting the user."	
+  	end
+  	redirect_to :back
   end
 end
+
+  private
+    def user_params
+      params.require(:user).permit(:fname, :lname, :email, :password)
+     end
